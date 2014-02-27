f44c5b997bb44bb30cd49bf61e9ed0deec8a7a4a
neo9988 <aneo9988@hotmail.com>
2014-02-27 01:20:00 -0500

    Second Commit

--- radio.cpp
+++ radio.cpp
@@ -2,5 +2,11 @@
 //Created By: Avita Sharma 
 //On: 02/07/2014
 
-cout << "You are... a radio star"<< endl;
+#include <iostream>
+using namespace std;
 
+int main(){
+	cout << "You are... a radio star"<< endl;
+	return 0;
+
+}
